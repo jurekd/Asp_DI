7d94b10c50173fe5517922326bdc75fea680e52e
Jurek <Jurek@OWCZARY-XEON>
2018-04-16 20:48:06 +0200

    Working with Ninject MVC3

---
+++ EssentialTools/EssentialTools/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using EssentialTools.Models;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EssentialTools.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IValueCalculator calc;
+
+        private Product[] products = {
+        new Product {Name = "Kajak", Category="Sporty wodne", Price = 275M},
+        new Product {Name = "Kamizelka ratunkowa", Category="Sporty wodne", Price = 48.95M},
+        new Product {Name = "Piłka nożna", Category="Piłka nożna", Price = 19.50M},
+        new Product {Name = "Flaga narożna", Category="Piłka nożna", Price = 34.95M}
+    };
+
+        public HomeController(IValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+
+        public ActionResult Index()
+        {
+            ShoppingCart cart = new ShoppingCart(calc) { Products = products };
+            decimal totalValue = cart.CalculateProductTotal();
+            return View(totalValue);
+        }
+    }
+}
